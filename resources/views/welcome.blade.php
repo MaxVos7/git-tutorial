8037ac87fcf10838de63af5b5b0fc5a5753e5529
Max Vos <max.vos7@gmail.com>
2018-12-02 11:02:50 +0100

    update home page for new feature

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    Laravel 37
                 </div>
 
                 @include('stats')
